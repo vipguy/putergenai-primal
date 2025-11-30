3cc1d96977cdfb48da7ff18bbac8ae2033b80f3a
s0mple <92299625+Nerve11@users.noreply.github.com>
2025-11-30 14:37:45 +0400

    Update __init__.py
    bump version to 2.0.2

--- src/putergenai/__init__.py
+++ src/putergenai/__init__.py
@@ -4,6 +4,7 @@ PuterGenAI - Asynchronous Python client for the Puter.com API.
 
 from .putergenai import PuterClient
 
-__version__ = '2.0.1'
+__version__ = '2.0.2'
 
 __all__ = ["PuterClient", "__version__"]
+
