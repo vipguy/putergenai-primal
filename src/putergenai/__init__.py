36bcb3c1a64770fa86737255cc65136a874f6835
s0mple <92299625+Nerve11@users.noreply.github.com>
2025-12-06 23:06:38 +0400

    Update __init__.py

--- src/putergenai/__init__.py
+++ src/putergenai/__init__.py
@@ -4,7 +4,6 @@ PuterGenAI - Asynchronous Python client for the Puter.com API.
 
 from .putergenai import PuterClient
 
-__version__ = '2.0.2'
+__version__ = '2.1.0'
 
 __all__ = ["PuterClient", "__version__"]
-
